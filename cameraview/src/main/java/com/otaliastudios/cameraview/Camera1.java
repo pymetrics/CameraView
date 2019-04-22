9f353d7e085eb8cc0ed804d32ccb450ab0cd5e39
Neha Rege <neha@pymetrics.com>
2019-04-22 15:10:53 -0400

    changing captured video size based on the preview

--- cameraview/src/main/java/com/otaliastudios/cameraview/Camera1.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/Camera1.java
@@ -704,7 +704,7 @@ class Camera1 extends CameraController implements Camera.PreviewCallback, Camera
         CamcorderProfile profile = getCamcorderProfile();
         mMediaRecorder.setOutputFormat(profile.fileFormat);
         mMediaRecorder.setVideoFrameRate(30);
-        mMediaRecorder.setVideoSize(profile.videoFrameWidth, profile.videoFrameHeight);
+        mMediaRecorder.setVideoSize(mPreview.getDesiredSize().getWidth(), mPreview.getDesiredSize().getHeight());
         if (mVideoCodec == VideoCodec.DEFAULT) {
             mMediaRecorder.setVideoEncoder(profile.videoCodec);
         } else {
